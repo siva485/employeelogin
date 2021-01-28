78f2b957f1001c602edc0062edd0ba08ab8f7b67
siva485 <paturisiva485@gmail.com>
2021-01-29 01:27:24 +0530

    * is added

--- employeelogin/urls.py
+++ employeelogin/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from loginapp import views
+from django.conf.urls import url
 
 urlpatterns = [
+    url('^$', views.indexPage),
     path('admin/', admin.site.urls),
-    path(r'^$', views.indexPage),
     path('index/', views.indexPage),
+
+
 ]
